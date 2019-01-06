d98521992c24f51a7de116850c8ad8fa4bdb79df
Heber Nunomura <heber28@gmail.com>
2019-01-06 17:40:24 -0200

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pesquisa::Application.config.secret_token = 'c2c529375fe50502619f7e5b9d0827b35ef4eb22fd4ede2ad459ea33acadf7a22dbe26e71cd74c98d8fa842d9b0894fec81cda0627e773328e855340d0a3fc39'
